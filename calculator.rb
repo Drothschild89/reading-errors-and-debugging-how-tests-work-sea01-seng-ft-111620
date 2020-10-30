680d7be4984cfdb4ff43d5426de701e74ccd136c
Dustin Rothschild <drothschild89@gmail.com>
2020-10-30 06:46:56 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,2 @@
 first_number = 5
-second_number = 2 
-sum = first_number + second_number
-difference = first_number - second_number
-product = first_number * second_number
-quotient = first_Number / second_number
+second_number = 5
